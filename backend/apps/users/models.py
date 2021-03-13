337d43e40b3b134907a8614fbf0ac689204c3b04
jay <ckj9014@gmail.com>
2021-03-13 16:31:28 +0900

    [apps] [users] user 모델 추가 (AbstractUser 상속)

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import models as auth_models
 from django.db import models
 
-# Create your models here.
+
+class User(auth_models.AbstractUser):
+    name = models.CharField(verbose_name='name', max_length=50)
+
+    class Meta:
+        verbose_name = 'user'
+
+    def __str__(self):
+        return "name: {}".format(self.username)
+
