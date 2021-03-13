71eeca4ef15b55af0a2425081e36bb9f0443704c
jay <ckj9014@gmail.com>
2021-03-14 01:46:05 +0900

    코드정리 (admin_url 및 admin config 추가, requirements 정리)

---
+++ backend/conf/settings/prod_admin.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+from backend.conf.settings.base import *
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'SECRET_KEY_GERNERATOR' # 배포시 변경해서 seret_key 넣어줌
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+ALLOWED_HOSTS = ['api.nrise.com'] # 실서버 호스트 (임의)
+
+ROOT_URLCONF = 'urls.admin_url'
+
+INSTALLED_APPS += {
+    'suit'
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+
+#########################################
+#            DataBase
+#########################################
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nrisedb',
+        'OPTIONS': {
+            'read_default_file': "PROD_MYSQL_CONFIG",
+            'charset': 'utf8mb4',
+            'init_command': 'set collation_connection=utf8mb4_unicode_ci; SET default_storage_engine=INNODB; SET SQL_MODE=STRICT_TRANS_TABLES;',
+        },
+        'CONN_MAX_AGE': 600,
+    },
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+# 제 로컬 DB 환경으로 맞췄습니다.
+USE_TZ = False
+USE_I18N = True
+USE_L10N = True
+TIME_ZONE = 'Asia/Seoul'
+LANGUAGE_CODE = 'ko-kr'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = '/static/'
--- backend/urls/admin_url.py
+++ backend/urls/admin_url.py
@@ -1,6 +1,5 @@
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('', admin.site.urls),
