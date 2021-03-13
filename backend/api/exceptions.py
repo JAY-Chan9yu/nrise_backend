0ebbf1564ff2900d9be7a6906dccc665077fd8cf
jay <ckj9014@gmail.com>
2021-03-14 01:04:55 +0900

    [api] 커스텀 예외, 페이지네이션 클래스 추가

---
+++ backend/api/exceptions.py
@@ -0,0 +1,7 @@
+from rest_framework.exceptions import APIException
+
+
+class WithdrawalUser(APIException):
+    status_code = 403
+    default_detail = 'This user has already been unsubscribed.'
+    default_code = 'WITHDRAWAL'
