fb698c013f5b0c9c7e9afca39c173154ccabd3e2
jay <ckj9014@gmail.com>
2021-03-14 01:04:30 +0900

    [users] [models] User 모델 수정 (username 필드 null=True, withdrawal_usernam 필드 추가(회원탈퇴시 username 저장)

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import models as auth_models
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -6,9 +7,23 @@ from apps.core.models import TimeModelMixin
 
 
 class User(TimeModelMixin, auth_models.AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        null=True,
+        default=None,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
     name = models.CharField(verbose_name='name', max_length=50)
     last_logout = models.DateTimeField(_('last logout'), blank=True, null=True)
     withdrawal = models.DateTimeField(_('withdrawal'), blank=True, null=True) # 회원탈퇴 시간
+    withdrawal_username = models.CharField(_('withdrawal_username'), max_length=150, null=True, default=None) # 탈퇴한 ID
 
     class Meta:
         verbose_name = 'user'
@@ -28,3 +43,4 @@ class UserSession(TimeModelMixin):
 
     class Meta:
         verbose_name = 'user session'
+        ordering = ('-id',)
